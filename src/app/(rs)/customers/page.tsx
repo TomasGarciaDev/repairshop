f854b90403427ea294fbde92533c6f6b0b48ec69
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-02-13 12:59:16 +0100

    Feat: Sentry span for queries

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,5 +1,6 @@
 import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import * as Sentry from "@sentry/nextjs";
 
 export const metadata = {
   title: "Customers Search",
@@ -14,7 +15,11 @@ export default async function Customers({
 
   if (!searchText) return <CustomerSearch />;
 
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-1",
+  });
   const results = await getCustomerSearchResults(searchText);
+  span.end();
 
   return (
     <>
