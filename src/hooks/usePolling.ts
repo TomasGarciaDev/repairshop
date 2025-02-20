35ea2e3859d55d91e89c84be31f096f5f22ad8ae
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-02-20 13:58:09 +0100

    Update: usePolling params naming fix

--- src/hooks/usePolling.ts
+++ src/hooks/usePolling.ts
@@ -1,21 +1,18 @@
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function usePolling(
-  searchParams: string | null,
-  ms: number = 60000
-) {
+export function usePolling(searchParam: string | null, ms: number = 60000) {
   const router = useRouter();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      console.log("Interval running");
-      if (!searchParams) {
+      console.log("interval running");
+      if (!searchParam) {
         console.log("refreshing data");
         router.refresh();
       }
     }, ms);
 
     return () => clearInterval(intervalId);
-  }, [searchParams, ms]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [searchParam, ms]); // eslint-disable-line react-hooks/exhaustive-deps
 }
